f021f781b0f37a3fc9691984cc8c818b73823bc5
Dominique FABRE <dfabre@clever-age.com>
2019-01-17 11:39:57 +0100

    Prepare publishing

--- demo/src/components/Yolo.js
+++
@@ -1,6 +0,0 @@
-import { withTransmorpher } from '../transmorpher'
-
-export default withTransmorpher({
-  query: '[data-react=root] input[type=text]',
-  key: 'TheInput',
-})(props => props.renderTag())
